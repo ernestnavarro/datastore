7ec707507ed78eaaf9ce755c32407b1ade451333
ernestnavarro <ernest.navarro@intechsive.com>
2015-03-31 17:11:20 +0800

    Create datastore2.php

---
+++ datastore2.php
@@ -0,0 +1,27 @@
+<?php
+  2 
+  3 class Mysql {
+  4 
+  5         public function query(
+  6                 $table,
+  7                 $limit,
+  8                 $page,
+  9                 Array $where,
+ 10                 Array $return_fields
+ 11         )
+ 12         {
+ 13 
+ 14                 $sql = "select  from {$table} where ";
+ 15 
+ 16                 return $data;
+ 17         }
+ 18 
+ 19         public function add()
+ 20         {
+ 21             $sql = "select from "
+ 22         }
+ 23 
+ 24 }
+ 25 
+ 26 ?>
+  
