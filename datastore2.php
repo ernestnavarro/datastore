2c7eb82ca362a8ec0587b59a0acb6b1f3b790910
ernestnavarro <ernest.navarro@intechsive.com>
2015-04-01 16:59:36 +0800

    datastore2.php edited

--- datastore2.php
+++ datastore2.php
@@ -1,36 +1,47 @@
 <?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "datastore";
+
+try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    // sql to create table
+                        $sql = "CREATE TABLE Invoice (
+                            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                                 VARCHAR(30) NOT NULL,
+                                    ContactId VARCHAR(30) NOT NULL,
+                                        Paystatus VARCHAR(50),
+                                            reg_date TIMESTAMP
+                                                )";
+
+                                                    // use exec() because no results are returned
+                                                        $conn->exec($sql);
+                                                            echo "Table MyGuests created successfully";
+                                                                }
+                                                                catch(PDOException $e)
+                                                                    {
+                                                                            echo $sql . "<br>" . $e->getMessage();
+                                                                                }
+                                                                                $conn = null;
+        public class Mysql {
    
-   class Mysql {
-   
-          public function query(
-                   $table,
-                   $limit,
-                   $page,
-                   Array $where,
-                  Array $return_fields
-          )
-          {
-  
-                  $sql = "select  from {$table} where ";
-  
-                  return $data;
-          }
-  
-          public function add()
-          {
-              $sql = "select from "
-          }
- 
-          public function update()
-          {
-              $sql = "select from "
-          }
-  
-          public function validate()
-          {
-              $sql = "select from "
-          }
+        public function mysql_insert($table, $inserts) {
+            $values = array_map('mysql_real_escape_string', array_values($inserts));
+            $keys = array_keys($inserts);
+                    
+                return mysql_query('INSERT INTO `'.$table.'` (`'.implode('`,`', $keys).'`) VALUES (\''.implode('\',\'', $values).'\')');
+}
+
+                 mysql_insert('Invoice', array(
+                              'ContactId' =>$contact_id,
+                              'PayStatus' => 1,
+                              'RefundStatus' => 0,
+                             ));
  }
- 
+
  ?>
-  
